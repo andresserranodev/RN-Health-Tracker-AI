db4567b44f9f5770769d3028369d9d5d12c14f8a
Andres David Serrano <ads.f117@gmail.com>
2025-07-25 15:42:23 -0500

    Use environment variable for Gemini API key
    Replaced hardcoded Gemini API key with process.env.EXPO_PUBLIC_GEMINI_API_KEY in geminiApiClient.ts for improved security. Added HealthTracker/.env to .gitignore to prevent sensitive data from being committed.

--- HealthTracker/src/api/geminiApiClient.ts
+++ HealthTracker/src/api/geminiApiClient.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-// TODO
-//const API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY;
-const API_KEY = `AIzaSyCCzgHCm9VNsedikWjZrH0HWgJNzFV34aw`;
-
+const API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY;
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-preview-image-generation:generateContent?key=${API_KEY}`;
 
 const geminiApiClient = axios.create({
